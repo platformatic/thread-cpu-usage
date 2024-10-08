8c4088fc1e42f817367c97f6be14ce5a4175e208
Paolo Insogna <paolo@cowtech.it>
2024-10-08 15:34:09 +0200

    feat: Allow publishing from source.

---
+++ scripts/install.js
@@ -0,0 +1,19 @@
+import { spawn } from 'node:child_process'
+import { once } from 'node:events'
+import { existsSync } from 'node:fs'
+import { arch, platform } from 'node:os'
+import { resolve } from 'node:path'
+
+const libc = existsSync('/etc/alpine-release') ? 'musl' : 'glibc'
+const addonPath = resolve(import.meta.dirname, '../native', `${platform()}-${arch()}-${libc}.node`)
+
+if (process.env.npm_config_build_from_source === 'true' || !existsSync(addonPath)) {
+  const subprocess = spawn('npm', ['run', 'build'], { stdio: 'inherit' })
+
+  const [code] = await once(subprocess, 'exit')
+
+  if (code !== 0) {
+    console.error(`Building from source failed with error code ${code}.`)
+    process.exit(code)
+  }
+}
