d94de7c08188fa3c032076226ef83465263afb44
Paolo Insogna <paolo@cowtech.it>
2024-10-09 11:21:58 +0200

    fix: Fixed cleaning on Docker.

--- scripts/postbuild.js
+++ scripts/postbuild.js
@@ -2,6 +2,11 @@ import { cp, rm } from 'node:fs/promises'
 import { resolve } from 'node:path'
 import { cleanNativeDirectory, getNativeAddonPath } from './helper.js'
 
-await cleanNativeDirectory()
+try {
+  await cleanNativeDirectory()
+} catch (e) {
+  // This can fail on Docker, and it is okay
+}
+
 await cp(resolve(import.meta.dirname, '../build/Release/thread-cpu-usage-native.node'), getNativeAddonPath())
 await rm(resolve(import.meta.dirname, '../build'), { recursive: true, force: true })
